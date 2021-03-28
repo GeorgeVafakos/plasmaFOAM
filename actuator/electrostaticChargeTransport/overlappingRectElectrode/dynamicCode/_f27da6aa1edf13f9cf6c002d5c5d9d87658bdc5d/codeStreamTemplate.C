a3aedbdf14588cc5ced011ef3d46957a18527ab2
GeorgeVafakos <george.vafakos@gmail.com>
2021-03-29 00:32:49 +0300

    Built actuator case and add tutorials

---
+++ actuator/electrostaticChargeTransport/overlappingRectElectrode/dynamicCode/_f27da6aa1edf13f9cf6c002d5c5d9d87658bdc5d/codeStreamTemplate.C
@@ -0,0 +1,77 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_f27da6aa1edf13f9cf6c002d5c5d9d87658bdc5d
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 63 "/home/george/OpenFOAM/george-7/plasmaFOAM/actuator/electrostaticChargeTransport/overlappingRectElectrode/system/blockMeshDict.#codeStream"
+scalar suma = 0;
+        int i = 0;
+        for (i=0 ; i<40 ; i++)
+            suma += pow(1.000000e+03 , (i/(40 - 1.0)) );
+        os << 1.965000e+00/suma << endl;
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
