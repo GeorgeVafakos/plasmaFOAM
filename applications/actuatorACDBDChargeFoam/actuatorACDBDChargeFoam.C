dbe2a14c2c8073943a2aa66d3186f559a53e1206
George Vafakos <george.vafakos@gmail.com>
2022-05-09 17:51:54 +0300

    Add AC-DBD case

--- applications/actuatorACDBDChargeFoam/actuatorACDBDChargeFoam.C
+++ applications/actuatorACDBDChargeFoam/actuatorACDBDChargeFoam.C
@@ -131,7 +131,7 @@ int main(int argc, char *argv[])
         #include "resetStreamer.H"
         #include "writeCustomTime.H"
 
-
+        // Comment
         // runTime.write();
 
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
