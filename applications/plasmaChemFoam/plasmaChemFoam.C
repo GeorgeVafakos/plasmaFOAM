da7c68d206c8533a712c0bbf3adbc2c15bd95ebb
GeorgeVafakos <george.vafakos@gmail.com>
2022-10-03 01:01:59 +0300

    Add chemistry

--- applications/plasmaChemFoam/plasmaChemFoam.C
+++ applications/plasmaChemFoam/plasmaChemFoam.C
@@ -46,47 +46,122 @@ int main(int argc, char *argv[])
 
     Info<< "\nStarting iteration loop\n" << endl;
 
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+    // Equations for the External Electric Field
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    // Reset counters
+    counter = 0;
+    solverPerformance::debug = 1;
+
+    while ((convVoltAext>0 || convVoltDext>0 ) && counter<100000)
+    {
+        // Air
+        Foam::solverPerformance solvPerfVoltAext = solve 
+        (
+            fvm::laplacian(voltAext)
+        );
+        convVoltAext = solvPerfVoltAext.nIterations();
+
+        // Dielectric
+        Foam::solverPerformance solvPerfVoltDext = solve
+        (
+            fvm::laplacian(voltDext)
+        );
+        convVoltDext = solvPerfVoltDext.nIterations();
+
+        counter++;
+        solverPerformance::debug = 0;
+        if (counter % 50 == 0)
+        {
+            Info<< "Current Loop = " << counter << endl;
+            solverPerformance::debug = 1;
+        }
+    }
+
+    Info<< "External Field: Region Inner Loops = " << counter << endl;
+
+    // Calculate the external electric field
+    EAext = -fvc::grad(voltAext);
+    EDext = -fvc::grad(voltDext);
+
     while (runTime.loop())
     {
         Info<< "Iteration = " << runTime.timeName() << nl << endl;
 
+
         // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-        // Equations for the Electric Field
+        // Equations for the Induced Electric Field
         // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-        // // Reset counters
-        // counter = 0;
-        // solverPerformance::debug = 1;
+        // Reset counters
+        counter = 0;
+        solverPerformance::debug = 1;
 
-        // while ((convVoltAext>0 || convVoltDext>0 ) && counter<1)
-        // {
+        while ((convVoltArho>0 || convVoltDrho>0 ) && counter<100000)
+        {
             // Air
-            Foam::solverPerformance solvPerfVoltA = solve 
+            Foam::solverPerformance solvPerfVoltArho = solve 
             (
-                fvm::laplacian(voltA)
+                fvm::laplacian(voltArho) + (e/epsilon0)*(ni)
             );
-            convVoltA = solvPerfVoltA.nIterations();
+            convVoltArho = solvPerfVoltArho.nIterations();
 
             // Dielectric
-            Foam::solverPerformance solvPerfVoltD = solve
+            Foam::solverPerformance solvPerfVoltDrho = solve
             (
-                fvm::laplacian(voltD)
+                fvm::laplacian(voltDrho)
             );
-            convVoltD = solvPerfVoltD.nIterations();
+            convVoltDrho = solvPerfVoltDrho.nIterations();
 
-        //     counter++;
-        //     solverPerformance::debug = 0;
-        //     if (counter % 5000 == 0)
-        //     {
-        //         Info<< "Current Loop = " << counter << endl;
-        //         solverPerformance::debug = 1;
-        //     }
-        // }
+            counter++;
+            solverPerformance::debug = 0;
+            if (counter % 50 == 0)
+            {
+                Info<< "Current Loop = " << counter << endl;
+                solverPerformance::debug = 1;
+            }
+        }
 
-        // Info<< "External Field: Region Inner Loops = " << counter << endl;
+        Info<< "Induced Field: Region Inner Loops = " << counter << endl;
 
+        // Calculate total electric potential
+        voltA = voltAext + voltArho;
+        voltD = voltDext + voltDrho;
 
+        // Calculate the induced electric field
+        EArho = -fvc::grad(voltArho);
+        EDrho = -fvc::grad(voltDrho);
 
+        // Total electric field
+        EA = EAext + EArho;
+        ED = EDext + EDrho;
+
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+        // Specied continuity equation
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+        // Update face fluxes
+        eleFlux =  mue*mesh.magSf()*fvc::snGrad(voltA);
+        ionFlux = -mui*mesh.magSf()*fvc::snGrad(voltA);
+
+        // Solve the electron continuity
+        fvScalarMatrix eleEqn
+        (
+            fvm::ddt(ne) + fvm::div(eleFlux, ne) - fvm::laplacian(De, ne) 
+            ==
+            fvm::Sp(alpha*mag(-mue*EA), ne) - fvm::Sp(r*ni, ne)
+        );
+        eleEqn.solve();
+
+        // Solve the ion continuity
+        fvScalarMatrix ionEqn
+        (
+            fvm::ddt(ni) + fvm::div(ionFlux, ni) - fvm::laplacian(Di, ni) 
+            ==
+            alpha*mag(-mue*EA)*ne - fvm::Sp(r*ne, ni)
+        );
+        ionEqn.solve();
 
 
         runTime.write();
@@ -95,6 +170,8 @@ int main(int argc, char *argv[])
             << "  ClockTime = " << runTime.elapsedClockTime() << " s"
             << nl << endl;
     }
+
+    runTime.writeAndEnd();
 
     Info<< "End\n" << endl;
 
